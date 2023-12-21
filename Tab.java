49a17d5a5a4ad07021856ae2b393bab165d82dea
karan hareshwar sankhe <sankhe00009@gmail.com>
2023-12-21 12:38:07 +0530

    codes

---
+++ Tab.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Tab {
+    
+    int n=5;
+    int dp[]=new int[n+1];
+    Arrays.fill(dp,-1);
+    dp[0]=0;
+    dp[1]=1;
+
+
+    for(int i=2; i<=n; i++){
+        dp[i] = dp[i-1]+ dp[i-2];
+    }
+    System.out.println(dp[n]);
+    
+    
+  }
+
+
